a5d17323eacd6d1bddc74095412ea8c97ff7056e
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-02-20 20:58:56 +0200

    update

---
+++ module-6.js
@@ -0,0 +1,82 @@
+// !Prototype
+
+// Прототип создается с права налево
+const objC = {
+  z: 5,
+};
+
+console.log(objC);
+
+const objB = Object.create(objC); // objC прототип objB создает пустой обект,
+//  поэтому сразу после него можна записать свойство
+
+console.log(objB); // пустой обэкт
+console.log(objB.z);
+objB.y = 2;
+objB.z = 10;
+console.log(objB.z); // создает новое свойство, но не перезаписывает в прототипах
+console.log(objB); // будет новое значение { y: 2 }
+
+console.log("objB", objB.z); // так же можна достучатся до свойсва, если его нет на текуще обэкте,
+// то есть на прототипе.
+
+console.log(objB.hasOwnProperty("z")); // false ищет СОБСВЕННОЕ свойсво
+
+console.log(objB.sdfsdf); // получим undefined не потому что нет такого свойства, а его нет во всех цепочки
+
+// TODO hasOwnProperty показує чи властивість власна (тру), інакше (фолс)
+
+// ! К   Л   А   С
+
+// class Car {
+//   constructor(value) {
+// функции вызывается в контексте пустого обекта myCar
+//     this.a = value;
+//   }
+// }
+// 1. Создается пустой обект
+// const myCar = new Car(5); // Обект экземпляра класса Car;
+// console.log(myCar); // Car {}
+
+// ?Аргументи, які передаються при виклику new Car(), стають значеннями параметрів для методу constructor.
+
+// Аргументами можуть бути обэкт параметрів
+
+// const myCar = new Car({
+//   brand: "Audi",
+//   model: "Q3",
+//   price: 35000,
+// });
+
+// Тогда value - це параметр який є змінною обєкта {
+//     brand: 'Audi',
+//     model: 'Q3',
+//     price: 35000
+// }
+
+class Car {
+  constructor(value = { brand, model, price }) {
+    // функции вызывается в контексте пустого обекта myCar
+    this.brand = value.brand;
+    this.model = value.model;
+    this.price = value.price;
+  }
+}
+
+// !можна зробити деструктиризацію
+// class Car {
+//   constructor({ brand, model, price }) {
+// функции вызывается в контексте пустого обекта myCar
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+const myCar = new Car({
+  brand: "Audi",
+  model: "Q3",
+  price: 35000,
+});
+
+console.log(myCar);
---
+++ task-codewars.js
@@ -0,0 +1,9 @@
+// ?19.02.2024 13:30
+// TODO task 1
+// You will be given an array which will have three values(tail, body, head).
+// It is your job to re - arrange the array so that the animal is the right way round(head, body, tail).
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
+
+// !Метод изменяем масив, а не создает новый.
