ff4ebefd0c8e6953317a03a1cbe82f9670a374db
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-03-05 23:00:14 +0200

    module-8

--- module-7/module-7lms.js
+++ module-7/module-7lms.js
@@ -1,12 +1,59 @@
-const articleRef = document.querySelector("article");
-console.log(articleRef.innerHTML);
+// const articleRef = document.querySelector("article");
+// console.log(articleRef.innerHTML);
 
 // articleRef.innerHTML = "";
-const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
-const ulref = document.querySelector(".list");
-const markup = technologies
-  .map((technologie) => `<li class="link-item">${technologie}</li>`)
-  .join("");
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const ulref = document.querySelector(".list");
+// const markup = technologies
+//   .map((technologie) => `<li class="link-item">${technologie}</li>`)
+//   .join("");
 // console.log(markup);
-console.log(ulref);
-ulref.innerHTML = markup;
+// console.log(ulref);
+// ulref.innerHTML = markup;
+
+// const btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", onButtonClick);
+
+// function onButtonClick(event) {
+//   console.log("event: ", event);
+//   console.log("event type: ", event.type);
+//   console.log("currentTarget: ", event.currentTarget);
+// }
+
+const cleanBtn = document.querySelector(".js-clear");
+const logList = document.querySelector(".log-list");
+let keypressCounter = 1;
+
+console.log(cleanBtn);
+console.log(logList);
+
+document.addEventListener("keydown", logMessage);
+document.addEventListener("keyup", logMessage);
+cleanBtn.addEventListener("click", reset);
+
+function logMessage({ type, key, code }) {
+  const markup = `<div class="log-item">
+    <span class="chip">${keypressCounter}</span>
+    <ul>
+      <li><b>Event</b>: ${type}</li>
+      <li><b>Key</b>: ${key}</li>
+      <li><b>Code</b>: ${code}</li>
+    </ul>
+  </div>`;
+
+  logList.insertAdjacentElement("afterbegin", markup);
+
+  if (type === "keyup") {
+    incrementKeypressCounter();
+  }
+}
+
+function reset() {
+  keypressCounter = 1;
+  logList.innerHTML = "";
+}
+
+function incrementKeypressCounter() {
+  keypressCounter += 1;
+}
